a3479dc3d8b7dd75f322303eaddb502c754ade4a
suparansharma <suparansharmap@gmail.com>
2024-03-05 00:31:07 +0600

    update done

--- components/Login/index.jsx
+++ components/Login/index.jsx
@@ -45,7 +45,7 @@ const SignInComponent = ({ submitForm, setEmail, setPassword, setLoading, loadin
                             type="email"
                             placeholder="Enter your email"
                             className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                            onChange={e => setEmail(e.target.value)}
+                           
                         />
 
                         <span className="absolute right-4 top-4">
